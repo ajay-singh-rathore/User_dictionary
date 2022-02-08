2911a0509f63c27b53cdb4d6048092fbedda933f
AJAY_SINGH_RATHORE <96069023+ajay-singh-rathore@users.noreply.github.com>
2022-02-08 20:33:56 +0530

    Add files via upload
    You can add key and values 
    key means word 
    values means meaning of the word

---
+++ User_Dictionary.py
@@ -0,0 +1,7 @@
+dict={"accidial":"dial someone's number on phone accidentally",
+      "bitcoin" :"an online payment system that does not require an intermediary",
+      "blog":"a regularly updated website or web page, typically one run by an individual or small group",
+      "bokeh":"visual quality of the out-of-focus areas of a photographic image, especially as rendered by a particular lens.",
+      "buko":"coconut, coconut water; a person who is less than 5ft tall and really angry"}
+user= str(input("ENter the word :  "))
+print(dict.get(user))
